56504159a8cd138e5b03c6d586d22a1d70937c65
uuunknownnn <44082777+uuunknownnn@users.noreply.github.com>
2023-01-24 20:30:07 +0200

    CBV updated

--- ToDoList/ToDoApp/models.py
+++ ToDoList/ToDoApp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.utils import timezone
+#import datetime
+from django.urls import reverse
 
-# Create your models here.
+# Create your models here.  timezone.now
 
 class Todo(models.Model):
     title=models.CharField(max_length=350)
+    content=models.TextField(default = '')
     complete=models.BooleanField(default=False)
+    date_posted = models.DateTimeField(default=timezone.now )
 
     def __str__(self):
         return(self.title)
+
+
+#overridding this method will redirect us to the detail template of the new task whenever created by the CreateView
+    def get_absolute_url(self):
+        return reverse('todo-detail', kwargs={'pk': self.pk})
--- ToDoList/ToDoApp/views.py
+++ ToDoList/ToDoApp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
-
 from .models import Todo
-# Create your views here.
-#from ToDoApp import base
+
+
+"""
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'ToDoApp/base.html', {"todo_list": todos})
+
+
 
 
 @require_http_methods(["POST"])
@@ -26,3 +28,52 @@ def delete(request, todo_id):
     todo = Todo.objects.get(id = todo_id)
     todo.delete()
     return redirect("home")
+
+"""
+
+
+# CBV
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'ToDoApp/home.html'
+    context_object_name = 'todo_list'
+    ordering = ["-date_posted"]
+
+
+#overriding the get_context_data function to add more context:
+    def get_context_data(self, **kwargs):
+        context = super(TodoListView, self).get_context_data(**kwargs)
+        return context
+
+
+"""
+    class meta:
+        fields = ["title", "complete", "date_posted"]
+"""
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = 'ToDoApp/todo_detail.html'
+
+    def get_absolute_url(self):
+            return reverse('todo-detail',
+               kwargs={'pk': self.pk })
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "content"]
+   # success_url = '/'  <<<<------- this would redirect the browser to home page after a new post is created
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "complete", "content"]
+    success_url = '/'
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = '/'
