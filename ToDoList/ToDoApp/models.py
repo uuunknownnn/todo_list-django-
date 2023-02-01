c250b592210ffc458cfd580e825f6a02a6d73979
uuunknownnn <44082777+uuunknownnn@users.noreply.github.com>
2023-02-01 21:38:46 +0200

    app with finall features

--- ToDoList/ToDoApp/models.py
+++ ToDoList/ToDoApp/models.py
@@ -2,16 +2,13 @@ from django.db import models
 from django.utils import timezone
 #import datetime
 from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.  timezone.now
+
 
 class Todo(models.Model):
     title=models.CharField(max_length=350)
-<<<<<<< HEAD
     content=models.TextField(default = '')
-=======
-    content=models.TextField(default = 'No Content to Show!')
->>>>>>> 9f48758a9aade35f0799e1410fe760e4a0a7c8f3
     complete=models.BooleanField(default=False)
     date_posted = models.DateTimeField(default=timezone.now )
 
--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from User import views as User_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ToDoApp.urls')),
+    path('register/',User_views.register, name='register' ),
+    path('profile/',User_views.profile, name='profile' ),
+
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name="User/password_reset.html") , name='password_reset'),
+    path('password-reset_done/',auth_views.PasswordResetDoneView.as_view(template_name="User/password_reset_done.html") , name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="User/password_reset_confirm.html") , name='password_reset_confirm'),
+    path('password-reset_complete',auth_views.PasswordResetCompleteView.as_view(template_name="User/password_reset_complete.html") , name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
